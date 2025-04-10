fe7d0d796555c935858343f6a8a77c4fcdc8b5aa
Adeyinka Adedapo <yinkaadedapo@kubona.ng>
2025-04-10 17:04:48 +0100

    scrollService sorted

--- src/app/services/scroll.service.ts
+++ src/app/services/scroll.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScrollService {
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
-  scrollToTop() {
-    window.scrollTo(0, 0);
+  scrollToTop(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo(0, 0);
+    } else {
+      console.log('ScrollToTop not executed on server.');
+    }
   }
 }
